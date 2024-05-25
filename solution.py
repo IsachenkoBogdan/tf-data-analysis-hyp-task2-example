1a5d40a28976fcc822797035cc525bce618d6930
IsachenkoBogdan <61412323+IsachenkoBogdan@users.noreply.github.com>
2024-05-25 23:15:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 683820405 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.01
+    return mannwhitneyu(x, y).pvalue < alpha# Ваш ответ, True или False
