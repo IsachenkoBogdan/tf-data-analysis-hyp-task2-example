95eaa6d90673371df0bdb933e81740281656f91f
IsachenkoBogdan <61412323+IsachenkoBogdan@users.noreply.github.com>
2024-05-26 22:31:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from hyppo.ksample import MMD
 
 chat_id = 683820405 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 0.01
-    return ks_2samp(x, y).pvalue < alpha # Ваш ответ, True или False
+    return MMD(compute_kernel = "laplacian").test(x,y)[1] < alpha
